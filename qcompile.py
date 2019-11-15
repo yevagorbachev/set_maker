94166d56f7e8a02ce864435aa2b42351a96920c2
yevagorbachev <yevgeniy.gorbachev@gmail.com>
2019-11-14 19:02:20 -0500

    added column to tables; added format strings

--- qcompile.py
+++ qcompile.py
@@ -9,13 +9,31 @@ commands = {
     r'disp [0-9]':disp,
 }
 
+qformat = '''
+\\begin{center}
+    \\textbf{{{TYPE}}} \\\\ By: {author}
+\\end{center}
+{num}) {SUBJECT} \\textit{{{form}}} {question}
+\\\\{choices}
+\\\\ANSWER: {answer}
+'''
+
+choiceformat = '''
+W) {W}
+\\\\X) {X}
+\\\\Y) {Y}
+\\\\Z) {Z}
+'''
 __db__ = 'questions.db'
 c = sqlite3.connect(__db__)
 
-def cmd(arg):
+def cmd(arg: str):
     for pattern, func in commands:
+        if re.search(pattern, arg):
 
         
+def add()
+
 def tex(n):
     return
 
